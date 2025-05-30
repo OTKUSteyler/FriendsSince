31e3f111e44cf427b0498f0d83db5149aead600b
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-05-30 22:15:50 +0200

    Add files via upload

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,4 +1,9 @@
 import { Forms } from "@vendetta/ui/components";
+import { findInReactTree } from "@vendetta/utils";
+import { before } from "@vendetta/patcher";
+import { findByProps } from "@vendetta/metro";
+import { findByDisplayName } from "@vendetta/metro";
+import { findByDisplayNameAll } from "@vendetta/metro";
 const { FormText } = Forms;
 
 export default () => (
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,27 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { findByProps, findByName, findByStoreName } from "@vendetta/metro";
+import { after } from "@vendetta/patcher";
+import { findInReactTree } from "@vendetta/utils";
+import React from "react";
+import { Text } from "react-native"; // Make sure this is imported for <Text>
 
-export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
-}
+const { LazyActionSheet } = findByProps("openLazy", "hideActionSheet");
+const { getChannelId } = findByStoreName("SelectedGuildStore");
+const SimplifiedUserProfileContent = findByName("SimplifiedUserProfileContent");
+
+const ReviewSection = ({ userId }: { userId: string }) => (
+  <Text style={{ color: "white", padding: 10 }}>User ID: {userId}</Text>
+);
+
+export default () =>
+  after("type", SimplifiedUserProfileContent, (args, ret) => {
+    const profileSections = findInReactTree(ret, (r) =>
+      r?.type?.displayName === "View" &&
+      Array.isArray(r?.props?.children) &&
+      r.props.children.find(
+        (i) => i?.type?.name === "SimplifiedUserProfileAboutMeCard"
+      )
+    )?.props?.children;
+
+    const userId = args[0]?.user?.id;
+    profileSections?.push(<ReviewSection userId={userId} />);
+  });

3bf621503f7c756173dfa16ce77f42bd9532e9c1
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-07-10 14:32:33 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,47 @@
+import { findByName, findByStoreName } from "@vendetta/metro";
+import { after } from "@vendetta/patcher";
+import { findInReactTree } from "@vendetta/utils";
 import { React } from "@vendetta/metro/common";
-import { findByProps } from "@vendetta/metro";
-import { before } from "@vendetta/patcher";
 
-const { Text, View } = React;
+const { View, Text } = React;
+
+const RelationshipStore = findByStoreName("RelationshipStore");
+const SimplifiedUserProfileContent = findByName("SimplifiedUserProfileContent");
+
+const ReviewSection = ({ userId }: { userId: string }) => {
+  const status = (() => {
+    const relation = RelationshipStore?.getRelationship(userId);
+    if (relation === 1 || relation === 2) return "Friend";
+    if (relation === 3) return "Blocked";
+    if (relation === 0) return "Not friends";
+    return "Unknown";
+  })();
+
+  return (
+    <View style={{ padding: 10 }}>
+      <Text style={{ color: "white" }}>Status: {status}</Text>
+    </View>
+  );
+};
 
 let unpatch: () => void;
 
 export const onLoad = () => {
-  const MessageHeader = findByProps("MessageTimestamp", "MessageHeader");
+  unpatch = after("type", SimplifiedUserProfileContent, (args, ret) => {
+    const userId = args?.[0]?.user?.id;
 
-  unpatch = before("default", MessageHeader, ([props]: any) => {
-    props.children.push(
-      <View style={{ padding: 4 }}>
-        <Text style={{ color: "white", fontSize: 12 }}>👥 Friends since 2023</Text>
-      </View>
+    const aboutMeSection = findInReactTree(ret, (x) =>
+      x?.type?.displayName === "View" &&
+      Array.isArray(x?.props?.children) &&
+      x.props.children.some(
+        (i) => i?.type?.name === "SimplifiedUserProfileAboutMeCard"
+      )
     );
+
+    if (!aboutMeSection || !userId) return;
+
+    aboutMeSection.props.children.push(<ReviewSection userId={userId} />);
+    return ret;
   });
 };
 
